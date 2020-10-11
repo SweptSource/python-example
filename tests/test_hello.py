79e35b53adb579ae8817588ad7f61ae7ef9c1498
SweptSource <alekswoj1@gmail.com>
2020-10-11 17:00:27 +0200

    Update test_hello.py

--- tests/test_hello.py
+++ tests/test_hello.py
@@ -4,14 +4,14 @@ import hello
 def test_says_world():
     assert hello.say_what() == 'world'
 
-def test_says_world():
+def test_empty():
     assert len(hello.say_what()) != ''
     
-def test_says_world():
+def test_1():
     assert 1==1
 
-def test_says_world():
+def test_2():
     assert 2+2==4
  
-def test_says_world():
+def test_null():
     assert 2+2!=null
